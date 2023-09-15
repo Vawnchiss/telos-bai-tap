19c9962509150abc8d07d44200b9f907d36bee5d
Van Chi Nguyen <nguyenvanchi210@gmail.com>
2023-09-15 19:01:18 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ['./bai-tap/**/*.html'],
   theme: {
+    screens: {
+      tablet: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      laptop: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {
       fontFamily: {
         sans: ['inter'],
@@ -35,6 +45,23 @@ module.exports = {
             fontWeight: '400',
           },
         ],
+        Body2: [
+          '14px',
+          {
+            lineHeight: '24px',
+            fontWeight: '400',
+          },
+        ],
+        Subtitile1: [
+          '18px',
+          {
+            lineHeight: '28px',
+            fontWeight: '700',
+          },
+        ],
+      },
+      colors: {
+        Turquoise: '#2EC5CE',
       },
     },
   },
